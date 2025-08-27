75f3ccc8eeef34126e2cd35431d3c254e8801a90
sokinpui <sosokinpui@gmail.com>
2025-08-28 00:39:44 +0800

    add script to gen code

--- pb_gen.sh
+++ pb_gen.sh
@@ -1,1 +1,19 @@
-python -m grpc.tools.prooc --proto_path=./protos --python_out=./src/synapse --grpc_python_out=./src/synapse ./protos/generate.proto
+#!/bin/bash
+
+echo "Generating Python gRPC code from .proto files..."
+
+mkdir -pv ./src/synapse/protos
+mkdir -pv ./src/synapse/grpc
+touch ./src/synapse/grpc/__init__.py
+
+cd "./src/synapse" || exit
+
+python -m grpc_tools.protoc \
+  --proto_path=grpc=protos \
+  --python_out=. \
+  --grpc_python_out=. \
+  protos/*.proto
+
+echo "Generation complete."
+
+gsed -i 's/from grpc import/from \. import/' ./grpc/*_grpc.py
